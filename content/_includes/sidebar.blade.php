3bbee8f7c7b308bbefd3836e5dfb27e622c222d8
Miguel Piedrafita <soy@miguelpiedrafita.com>
2017-11-02 13:38:31 +0100

    Update sidebar

--- content/_includes/sidebar.blade.php
+++ content/_includes/sidebar.blade.php
@@ -8,4 +8,7 @@
     <li><a href="@url('/bulma')">Bulma</a></li>
     <li><a href="@url('/uikit3')">UIKit 3</a></li>
     <li><a href="@url('/primercss')">Primer CSS</a></li>
+    <li><a href="@url('/w3css')">W3CSS</a></li>
+    <li><a href="@url('/tachyons')">Tachyons</a></li>
+    <li><a href="@url('/tailwindcss')">Tailwind CSS</a></li>
 </ul>
